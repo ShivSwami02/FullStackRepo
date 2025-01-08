23b0a6997111cb6d0b7e82618faaf57a19aaa601
NCS-PC 4 <NCS-PC 4@DESKTOP-MJ058C9.bbrouter>
2025-01-08 16:04:45 +0530

    Add.java is created

---
+++ PRoj01/src/test/AddOfNum.java
@@ -0,0 +1,15 @@
+package test;
+
+public class AddOfNum {
+	public static long add(long a,long b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		int a=10,b=30;
+		System.out.println(add(a,b));
+		
+
+	}
+
+}
